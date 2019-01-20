bc8acb49bc8b4dfb01edc35caa9e1b75c1ee3795
Asgeir Nilsen <asgeir@fintlabs.no>
2019-01-20 20:53:39 +0100

    Configure WebClient using a fixed connection pool.

--- src/main/java/no/fint/graphql/ApplicationConfig.java
+++ src/main/java/no/fint/graphql/ApplicationConfig.java
@@ -3,7 +3,11 @@ package no.fint.graphql;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
 public class ApplicationConfig {
@@ -11,8 +15,21 @@ public class ApplicationConfig {
     @Value("${fint.endpoint.root:https://play-with-fint.felleskomponent.no}")
     private String rootUri;
 
+    /**
+     * Build a WebClient using the Spring-supplied builder and ReactorResourceFactory.
+     * Overrides the ConnectionProvider using a fixed Connection pool.
+     * This pool can be configured with the properties {@link reactor.netty.ReactorNetty#POOL_ACQUIRE_TIMEOUT}
+     * and {@link reactor.netty.ReactorNetty#POOL_MAX_CONNECTIONS}.
+     * @param builder WebClient.Builder provided by Spring
+     * @param factory ReactorResourceFactory provided by Spring
+     * @return configured WebClient.
+     */
     @Bean
-    public WebClient webClient() {
-        return WebClient.create(rootUri);
+    public WebClient webClient(WebClient.Builder builder, ReactorResourceFactory factory) {
+        factory.setConnectionProvider(ConnectionProvider.fixed("fint"));
+        return builder
+                .clientConnector(new ReactorClientHttpConnector(factory, HttpClient::secure))
+                .baseUrl(rootUri)
+                .build();
     }
 }
