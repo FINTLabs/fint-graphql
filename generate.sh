84076c58ba12eaae7fd3a3cc34e231aaa6ec6462
Asgeir Nilsen <asgeir@fintlabs.no>
2020-11-26 07:39:40 +0100

    Cache requests matching kodeverk URIs.

--- generate.sh
+++ generate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 docker run \
-  -v ${PWD}/src/main/resources/schema:/src/graphql/schema \
-  -v ${PWD}/src/main/java/no/fint/graphql/model:/src/graphql/model \
-  fint/graphql-cli:1.1.0 \
+  -v "${PWD}/src/main/resources/schema:/src/graphql/schema" \
+  -v "${PWD}/src/main/java/no/fint/graphql/model:/src/graphql/model" \
+  fint/graphql-cli:latest \
   generate --exclude Fravar
 
 echo Schema and models generated.  Please remember to clean up ${PWD}/src/main/{resources/schema,java/no/fint/graphql/model} before commit.
--- src/main/java/no/fint/graphql/WebClientRequest.java
+++ src/main/java/no/fint/graphql/WebClientRequest.java
@@ -1,13 +1,21 @@
 package no.fint.graphql;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.servlet.context.GraphQLServletContext;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.cache.CacheMono;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
@@ -16,19 +24,35 @@ import java.time.Duration;
 @Component
 public class WebClientRequest {
 
-    @Autowired
-    private WebClient webClient;
+    private final WebClient webClient;
+    private final Cache<HashCode, Object> cache;
+    private final HashFunction hashFunction;
 
-    public <T> Mono<T> get(String uri, Class<T> type, DataFetchingEnvironment dfe) {
-        return get(webClient.get().uri(uri), type, dfe);
+    public WebClientRequest(
+            WebClient webClient,
+            @Value("${fint.webclient.cache-spec:maximumSize=10000,expireAfterWrite=10m}") String cacheSpec) {
+        this.webClient = webClient;
+        cache = Caffeine.from(cacheSpec).build();
+        hashFunction = Hashing.murmur3_128();
     }
 
-    private <T> Mono<T> get(WebClient.RequestHeadersSpec<?> request, Class<T> type, DataFetchingEnvironment dfe) {
+    public <T> Mono<T> get(String uri, Class<T> type, DataFetchingEnvironment dfe) {
         String token = getToken(dfe);
         log.debug("Token: {}", token);
+        final WebClient.RequestHeadersSpec<?> request = webClient.get().uri(uri);
         if (token != null) {
             request.header(HttpHeaders.AUTHORIZATION, token);
         }
+        if (StringUtils.containsIgnoreCase(uri, "/kodeverk/systemid/")) {
+            HashCode key = hashFunction.newHasher().putString(token, Charsets.UTF_8).putString(uri, Charsets.UTF_8).hash();
+            return CacheMono
+                    .lookup(cache.asMap(), key, type)
+                    .onCacheMissResume(() -> get(request, type));
+        }
+        return get(request, type);
+    }
+
+    private <T> Mono<T> get(WebClient.RequestHeadersSpec<?> request, Class<T> type) {
         return request.retrieve()
                 .onStatus(HttpStatus::is4xxClientError, r -> Mono.empty())
                 .bodyToMono(type)
