91583fe55d6357f9b9255704e9718819c9314b95
Asgeir Nilsen <asgeir@fintlabs.no>
2021-08-30 13:24:08 +0200

    Merge properties for Person.

--- generate.sh
+++ generate.sh
@@ -6,4 +6,6 @@ docker run \
   fint/graphql-cli:latest \
   generate --exclude Fravar
 
+cp PersonService.txt src/main/java/no/fint/graphql/model/felles/person/PersonService.java
+
 echo Schema and models generated.  Please remember to clean up ${PWD}/src/main/{resources/schema,java/no/fint/graphql/model} before commit.
