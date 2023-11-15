4ed06c7369e83b0d5f605d4925e922e88d34837d
Hknots <Hknots99@gmail.com>
2023-11-15 15:16:13 +0100

    move to config

--- src/main/java/no/fint/graphql/config/ConnectionConfig.java
+++ src/main/java/no/fint/graphql/config/ConnectionConfig.java
@@ -1,7 +1,6 @@
 package no.fint.graphql.config;
 
 import lombok.extern.slf4j.Slf4j;
-import no.fint.graphql.ConnectionProviderSettings;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
