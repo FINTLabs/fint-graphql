01b050ee4e9e1c2d1929b0a2859e07c66649b5fe
alstad <anders.alstad@gmail.com>
2026-03-25 14:35:03 +0100

    feat: enabled detailed logging of HTTP request using Netty wiretap

--- src/main/java/no/fint/graphql/config/WebClientConfig.java
+++ src/main/java/no/fint/graphql/config/WebClientConfig.java
@@ -3,27 +3,22 @@ package no.fint.graphql.config;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ReactorResourceFactory;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import io.netty.handler.logging.LogLevel;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfig {
-
-    private static final Logger DOWNSTREAM_HTTP_LOG = LoggerFactory.getLogger("no.fint.graphql.downstream.http");
 
     @Value("${fint.endpoint.root:http://traefik.traefik-v2}")
     private String rootUrl;
@@ -42,7 +37,8 @@ public class WebClientConfig {
         factory.setConnectionProvider(connectionProvider);
         return builder
                 .clientConnector(new ReactorClientHttpConnector(factory, httpClient -> {
-                    HttpClient client = httpClient.secure();
+                    HttpClient client = httpClient.secure()
+                            .wiretap("no.fint.graphql.downstream.netty", LogLevel.TRACE, AdvancedByteBufFormat.TEXTUAL);
                     if (connectTimeoutMs > 0) {
                         client = client.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeoutMs);
                     }
@@ -61,7 +57,6 @@ public class WebClientConfig {
                 }))
                 .baseUrl(rootUrl)
                 .defaultHeader("Host", host)
-                .filter(logRequest())
                 .build();
     }
 
@@ -70,30 +65,5 @@ public class WebClientConfig {
         ReactorResourceFactory factory = new ReactorResourceFactory();
         factory.setUseGlobalResources(false);
         return factory;
-    }
-
-    private ExchangeFilterFunction logRequest() {
-        return ExchangeFilterFunction.ofRequestProcessor(request -> {
-            if (DOWNSTREAM_HTTP_LOG.isTraceEnabled()) {
-                DOWNSTREAM_HTTP_LOG.trace("Downstream HTTP request\n{}", formatRequest(request));
-            }
-            return Mono.just(request);
-        });
-    }
-
-    private String formatRequest(ClientRequest request) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(request.method())
-                .append(' ')
-                .append(request.url());
-
-        request.headers().forEach((name, values) ->
-                values.forEach(value -> builder.append('\n')
-                        .append(name)
-                        .append(": ")
-                        .append(value))
-        );
-
-        return builder.toString();
     }
 }
